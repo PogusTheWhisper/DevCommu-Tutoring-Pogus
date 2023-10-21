b74041015655604d1f8c7d030a8834d3b45e73f9
PogusTheWhisper <gusjangkid@gmail.com>
2023-10-21 13:49:01 +0700

    OOP Homework-pro

--- Homework-todo.py
+++ Homework-todo.py
@@ -15,8 +15,16 @@ class Todo:
     def list_hw(self):
         for task_info in self.tasks:
             print(f"{task_info['task']} : {task_info['description']}")
-
-homework = Todo("homework")
+            
+class Todo_pro(Todo):
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def count_hw(self):
+        print(len(self.tasks))
+        
+homework = Todo_pro("homework")
 homework.add("พะสะไท", "ทำอะไรก็ไม่รู้")
 homework.add("พะสะปะกิด", "ทำอะไรก็ไม่รู้2")
 homework.list_hw()
+homework.count_hw()
